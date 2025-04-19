c6b06697efb621a893e52e13b92c0c653814e164
Venus <venusyip@outlook.com>
2025-04-19 22:51:08 +0100

    Add page markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,29 @@
-export default function App() {}
+import './styles/reset.css';
+import './styles/scss/App.scss';
+import Form from './components/Form';
+
+export default function App() {
+  return (
+    <>
+      <header className='header'>
+        <img
+          src={require('./assets/images/logo-full.svg')}
+          alt='Logo'
+          className='header__logo'
+        />
+      </header>
+
+      <section className='intro'>
+        <h1 className='intro__heading'>
+          Your Journey to Coding Conf 2025 Starts Here!
+        </h1>
+
+        <p className='intro__description'>
+          Secure your spot at next year's biggest coding conference.
+        </p>
+      </section>
+
+      <Form />
+    </>
+  );
+}
---
+++ src/components/Form.jsx
@@ -0,0 +1,74 @@
+export default function Form() {
+  return (
+    <form className='form'>
+      <h2 className='avatar-upload__heading'>Upload Avatar</h2>
+
+      <label htmlFor='avatar-upload' className='avatar-upload__label'>
+        <span className='avatar-upload__image-container'>
+          <img
+            src={require('../assets/images/icon-upload.svg')}
+            alt='Upload'
+            className='avatar-upload__image'
+          />
+        </span>
+        <button type='button' className='avatar-upload__button'>
+          Remove image
+        </button>
+        <button type='button' className='avatar-upload__button'>
+          Change image
+        </button>
+        <span className='avatar-upload__text'>
+          Drag and drop or click to upload
+        </span>
+        <input
+          type='file'
+          accept='image/*'
+          id='avatar-upload'
+          className='form__input-field'
+        />
+      </label>
+
+      <div className='avatar-upload__info'>
+        <img
+          src={require('../assets/images/icon-info.svg')}
+          alt='Information'
+          className='avatar-upload__info-icon'
+        />
+        <p className='avatar-upload__info-text'>
+          Upload your photo {'('}JPG or PNG, max size: 500KB{')'}.
+        </p>
+      </div>
+
+      <label htmlFor='full-name' className='form__label'>
+        Full Name
+      </label>
+      <input
+        type='text'
+        autoComplete='name'
+        id='full-name'
+        className='form__input-field'
+      />
+
+      <label htmlFor='email-address' className='form__label'>
+        Email Address
+      </label>
+      <input
+        type='text'
+        autoComplete='email'
+        placeholder='example@email.com'
+        id='email-address'
+        className='form__input-field'
+      />
+
+      <label htmlFor='github-username' className='form__label'>
+        GitHub Username
+      </label>
+      <input
+        type='text'
+        placeholder='@yourusername'
+        id='github-username'
+        className='form__input-field'
+      />
+    </form>
+  );
+}
