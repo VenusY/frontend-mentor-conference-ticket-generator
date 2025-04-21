92a2bf317e6c1640ef237a248910dba9a3d29241
Venus <venusyip@outlook.com>
2025-04-21 17:41:08 +0100

    Refactor avatar upload section layout

--- src/components/AvatarUpload.jsx
+++ src/components/AvatarUpload.jsx
@@ -2,6 +2,13 @@ export default function AvatarUpload() {
   return (
     <>
       <label htmlFor='avatar-upload' className='avatar-upload__label'>
+        <input
+          type='file'
+          accept='image/*'
+          id='avatar-upload'
+          className='avatar-upload__input-field'
+        />
+
         <span className='avatar-upload__image-container'>
           <img
             src={require('../assets/images/icon-upload.svg')}
@@ -21,13 +28,6 @@ export default function AvatarUpload() {
         <span className='avatar-upload__text'>
           Drag and drop or click to upload
         </span>
-
-        <input
-          type='file'
-          accept='image/*'
-          id='avatar-upload'
-          className='avatar-upload__input-field'
-        />
       </label>
 
       <div className='avatar-upload__info'>
