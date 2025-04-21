93d518848c7dac2991f8f49e64ed2c29e0097860
Venus <venusyip@outlook.com>
2025-04-21 17:11:33 +0100

    Refactor form layout

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -43,36 +43,42 @@ export default function Form() {
         </p>
       </div>
 
-      <label htmlFor='full-name' className='form__label'>
-        Full Name
-      </label>
-      <input
-        type='text'
-        autoComplete='name'
-        id='full-name'
-        className='form__input-field'
-      />
+      <div className='form__container'>
+        <label htmlFor='full-name' className='form__label'>
+          Full Name
+        </label>
+        <input
+          type='text'
+          autoComplete='name'
+          id='full-name'
+          className='form__input-field'
+        />
+      </div>
 
-      <label htmlFor='email-address' className='form__label'>
-        Email Address
-      </label>
-      <input
-        type='text'
-        autoComplete='email'
-        placeholder='example@email.com'
-        id='email-address'
-        className='form__input-field'
-      />
+      <div className='form__container'>
+        <label htmlFor='email-address' className='form__label'>
+          Email Address
+        </label>
+        <input
+          type='text'
+          autoComplete='email'
+          placeholder='example@email.com'
+          id='email-address'
+          className='form__input-field'
+        />
+      </div>
 
-      <label htmlFor='github-username' className='form__label'>
-        GitHub Username
-      </label>
-      <input
-        type='text'
-        placeholder='@yourusername'
-        id='github-username'
-        className='form__input-field'
-      />
+      <div className='form__container'>
+        <label htmlFor='github-username' className='form__label'>
+          GitHub Username
+        </label>
+        <input
+          type='text'
+          placeholder='@yourusername'
+          id='github-username'
+          className='form__input-field'
+        />
+      </div>
 
       <button className='form__submit-button'>Generate My Ticket</button>
     </form>
