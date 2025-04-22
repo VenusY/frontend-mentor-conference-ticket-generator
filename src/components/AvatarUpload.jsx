fdd29d0960194b8c33456bcec5e93bd04c3478a4
Venus <venusyip@outlook.com>
2025-04-22 08:51:10 +0100

    Reorder elements of avatar upload section

--- src/components/AvatarUpload.jsx
+++ src/components/AvatarUpload.jsx
@@ -1,21 +1,15 @@
+import '../styles/scss/AvatarUpload.scss';
+
 export default function AvatarUpload() {
   return (
     <>
-      <label htmlFor='avatar-upload' className='avatar-upload__label'>
+      <label htmlFor='avatar-upload' className='avatar-upload__main-container'>
         <input
           type='file'
           accept='image/*'
           id='avatar-upload'
           className='avatar-upload__input-field'
         />
-
-        <span className='avatar-upload__image-container'>
-          <img
-            src={require('../assets/images/icon-upload.svg')}
-            alt='Upload'
-            className='avatar-upload__image'
-          />
-        </span>
 
         <button type='button' className='avatar-upload__button'>
           Remove image
@@ -24,6 +18,14 @@ export default function AvatarUpload() {
         <button type='button' className='avatar-upload__button'>
           Change image
         </button>
+
+        <span className='avatar-upload__icon-container'>
+          <img
+            src={require('../assets/images/icon-upload.svg')}
+            alt='Upload'
+            className='avatar-upload__icon'
+          />
+        </span>
 
         <span className='avatar-upload__text'>
           Drag and drop or click to upload
