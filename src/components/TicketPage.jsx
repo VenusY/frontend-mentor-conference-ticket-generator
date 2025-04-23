b079a0dc0d1b3172ac76fde54e98b849db0998a9
Venus <venusyip@outlook.com>
2025-04-23 23:03:12 +0100

    Add markup for ticket page

--- src/components/TicketPage.jsx
+++ src/components/TicketPage.jsx
@@ -1,1 +1,28 @@
-export default function TicketPage() {}
+import Header from './Header';
+import Ticket from './Ticket';
+
+export default function TicketPage() {
+  return (
+    <section className='ticket-page'>
+      <Header />
+
+      <section className='intro'>
+        <h1 className='intro__heading'>
+          Congrats,{' '}
+          <span className='intro__heading--highlighted'>Jonatan Kristof</span>!
+          Your ticket is ready.
+        </h1>
+
+        <p className='intro__description'>
+          We've emailed your tiket to{' '}
+          <span className='intro__description--highlighted'>
+            jonatan@email.com
+          </span>{' '}
+          and will send updates in the run up to the event.
+        </p>
+      </section>
+
+      <Ticket />
+    </section>
+  );
+}
