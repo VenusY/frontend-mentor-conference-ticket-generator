474b153f65c243fc79425aa23421dea17d2f4d57
Venus <venusyip@outlook.com>
2025-04-23 23:28:22 +0100

    Add ticket markup

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,1 +1,43 @@
-export default function Ticket() {}
+export default function Ticket() {
+  return (
+    <div className='ticket'>
+      <header className='ticket-header'>
+        <img
+          src={require('../assets/images/logo-mark.svg')}
+          alt='Logo'
+          className='ticket-header__logo'
+        />
+
+        <section className='ticket-header__text-section'>
+          <h2 className='ticket-header__heading'>Coding Conf</h2>
+          <p className='ticket-header__date-location'>
+            Jan 31, 2025 / Austin, TX
+          </p>
+        </section>
+      </header>
+
+      <main className='ticket-receiver'>
+        <img
+          src={require('../assets/images/image-avatar.jpg')}
+          alt='Avatar'
+          className='ticket-receiver__avatar'
+        />
+
+        <div className='ticket-receiver__text-section'>
+          <p className='ticket-receiver__name'>Jonatan Kristof</p>
+
+          <div className='ticket-receiver__github'>
+            <img
+              src={require('../assets/images/icon-github.svg')}
+              alt='GitHub logo'
+              className='ticket-receiver__github-logo'
+            />
+            <p className='ticket-receiver__github-username'>
+              @jonatankristof0101
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
