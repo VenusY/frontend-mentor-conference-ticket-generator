9d86768263eaedc691c279676175b6a29a191350
Venus <venusyip@outlook.com>
2025-04-26 22:03:55 +0100

    Add left and right container to ticket

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,43 +3,49 @@ import '../styles/scss/Ticket.scss';
 export default function Ticket() {
   return (
     <div className='ticket'>
-      <header className='ticket-header'>
-        <img
-          src={require('../assets/images/logo-mark.svg')}
-          alt='Logo'
-          className='ticket-header__logo'
-        />
+      <div className='ticket__left-container'>
+        <header className='ticket-header'>
+          <img
+            src={require('../assets/images/logo-mark.svg')}
+            alt='Logo'
+            className='ticket-header__logo'
+          />
 
-        <section className='ticket-header__text-section'>
-          <h2 className='ticket-header__heading'>Coding Conf</h2>
-          <p className='ticket-header__date-location'>
-            Jan 31, 2025 / Austin, TX
-          </p>
-        </section>
-      </header>
-
-      <main className='ticket-receiver'>
-        <img
-          src={require('../assets/images/image-avatar.jpg')}
-          alt='Avatar'
-          className='ticket-receiver__avatar'
-        />
-
-        <div className='ticket-receiver__text-section'>
-          <p className='ticket-receiver__name'>Jonatan Kristof</p>
-
-          <div className='ticket-receiver__github'>
-            <img
-              src={require('../assets/images/icon-github.svg')}
-              alt='GitHub logo'
-              className='ticket-receiver__github-logo'
-            />
-            <p className='ticket-receiver__github-username'>
-              @jonatankristof0101
+          <section className='ticket-header__text-section'>
+            <h2 className='ticket-header__heading'>Coding Conf</h2>
+            <p className='ticket-header__date-location'>
+              Jan 31, 2025 / Austin, TX
             </p>
+          </section>
+        </header>
+
+        <main className='ticket-receiver'>
+          <img
+            src={require('../assets/images/image-avatar.jpg')}
+            alt='Avatar'
+            className='ticket-receiver__avatar'
+          />
+
+          <div className='ticket-receiver__text-section'>
+            <p className='ticket-receiver__name'>Jonatan Kristof</p>
+
+            <div className='ticket-receiver__github'>
+              <img
+                src={require('../assets/images/icon-github.svg')}
+                alt='GitHub logo'
+                className='ticket-receiver__github-logo'
+              />
+              <p className='ticket-receiver__github-username'>
+                @jonatankristof0101
+              </p>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
+
+      <div className='ticket__right-container'>
+        <p className='ticket__number'>#01609</p>
+      </div>
     </div>
   );
 }
